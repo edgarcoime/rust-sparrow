79bad76866578736f34000a13d1a0f4a190527ff
Edgar Coime <e.coimeii@gmail.com>
2022-03-15 14:45:04 -0700

    refactor: create more idiomatic rust Neuron::propagate

--- libs/neural-network/src/lib.rs
+++ libs/neural-network/src/lib.rs
@@ -1,7 +1,5 @@
 mod errors;
 
-use errors::*;
-
 // A network is built from layers
 pub struct Network {
     layers: Vec<Layer>
@@ -37,27 +35,12 @@ impl Layer {
 
 impl Neuron {
     fn propagate(&self, inputs: &[f32]) -> f32 {
-        // TODO: Implement better error checking
-        // if inputs.len() != self.weights.len() {
-        //     return Err(Error::MismatchedInputSize {
-        //         got: inputs.len(),
-        //         expected: self.weights.len()
-        //     });
-        // }
-        assert_eq!(inputs.len(), self.weights.len());
-
-        let mut output = 0.;
-
-        for i in 0..inputs.len() {
-            output += inputs[i] * self.weights[i];
-        }
-
-        output += self.bias;
-
-        if output > 0. {
-            output
-        } else {
-            0.
-        }
+        let output = inputs
+            .iter()
+            .zip(&self.weights)
+            .map(|(input, weight)| input * weight)
+            .sum::<f32>();
+        
+        (self.bias + output).max(0.)
     }
 }
