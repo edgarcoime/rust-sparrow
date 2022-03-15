2555794c19ae5460a921a2637d165d0b17b794c3
Edgar Coime <e.coimeii@gmail.com>
2022-03-15 15:28:56 -0700

    feat: setup testing structure

--- libs/neural-network/src/tests.rs
+++ libs/neural-network/src/tests.rs
@@ -1,6 +1,16 @@
 use super::*;
+use approx::assert_relative_eq;
 use rand::SeedableRng;
 use rand_chacha::ChaCha8Rng;
+
+macro_rules! assert_almost_eq {
+    ($left:expr, $right:expr) => {
+        let left: f32 = $left;
+        let right: f32 = $right;
+
+        assert!((left - right).abs() < f32::EPSILON)
+    };
+}
 
 #[test]
 fn test() {
@@ -9,11 +19,34 @@ fn test() {
     let mut rng = ChaCha8Rng::from_seed(Default::default());
     let neuron = Neuron::random(&mut rng, 4);
 
-    assert_eq!(neuron.bias, -0.6255188);
-    assert_eq!(neuron.weights, &[
+    // https://floating-point-gui.de
+    // There can be floating point inaccuracies so in a large amount of cases
+    // floating points cannot be asserted as EXACTLY the same. 
+    // Therefore we need to ensure that based on the floating point epsilon that the two 
+    // floats are relatively close enough to be the same.
+    assert_relative_eq!(neuron.bias, -0.6255188);
+    assert_relative_eq!(neuron.weights.as_slice(), &[
         0.67383957, 
         0.8181262, 
         0.26284897, 
         0.5238807
-    ]);
+    ].as_ref());
+}
+
+mod random {
+    use super::*;
+
+    #[test]
+    fn test() {
+        todo!()
+    }
+}
+
+mod propagate {
+    use super::*;
+
+    #[test]
+    fn test() {
+        todo!()
+    }
 }
