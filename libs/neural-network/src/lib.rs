943a750427a0e1cb682bbb8d036ce2d7a420f3f8
Edgar Coime <e.coimeii@gmail.com>
2022-03-15 14:59:38 -0700

    feat: naive approach to implementing Network::random

--- libs/neural-network/src/lib.rs
+++ libs/neural-network/src/lib.rs
@@ -1,5 +1,9 @@
 mod errors;
 
+pub struct LayerTopology {
+    neurons: usize,
+}
+
 // A network is built from layers
 pub struct Network {
     layers: Vec<Layer>
@@ -17,7 +21,25 @@ struct Neuron {
 }
 
 impl Network {
-    pub fn propagate(&self, mut inputs: Vec<f32>) -> Vec<f32> {
+    pub fn random(layers: Vec<LayerTopology>) -> Self {
+        assert!(layers.len() > 1);
+
+        let mut built_layers = Vec::new();
+
+        for i in 0..(layers.len() - 1) {
+            let input_neurons = layers[i].neurons;
+            let output_neurons = layers[i + 1].neurons;
+
+            built_layers.push(Layer::random(
+                input_neurons,
+                output_neurons,
+            ));
+        }
+
+        Self { layers: built_layers }
+    }
+
+    pub fn propagate(&self, inputs: Vec<f32>) -> Vec<f32> {
         self.layers
             .iter()
             .fold(inputs, |inputs, layer| layer.propagate(inputs))
@@ -25,6 +47,10 @@ impl Network {
 }
 
 impl Layer {
+    fn random(input_neurons: usize, output_neurons: usize) -> Self {
+        todo!()
+    }
+
     fn propagate(&self, inputs: Vec<f32>) -> Vec<f32> {
         self.neurons
             .iter()
