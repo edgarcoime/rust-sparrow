d97dfba6669ab02e7a6b27dcfb6aba667b3f8ad0
Edgar Coime <e.coimeii@gmail.com>
2022-03-15 12:42:27 -0700

    feat: setup structure for neural-network

--- libs/neural-network/src/lib.rs
+++ libs/neural-network/src/lib.rs
@@ -1,3 +1,26 @@
+// A network is built from layers
+pub struct Network {
+    layers: Vec<Layer>
+}
+
+// A layer is built from neurons
+struct Layer {
+    neurons: Vec<Neuron>
+}
+
+// A Neuron contains biases and output weights
+struct Neuron {
+    bias: f32,
+    weights: Vec<f32>,
+}
+
+
+impl Network {
+    pub fn propagate(&self, inputs: Vec<f32>) -> Vec<f32> {
+        todo!()
+    }
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
