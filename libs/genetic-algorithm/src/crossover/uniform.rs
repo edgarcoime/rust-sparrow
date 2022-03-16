7ac2f5e29d8a4f88a05b8169ad4dfae5e97b00a3
Edgar Coime <e.coimeii@gmail.com>
2022-03-15 20:21:43 -0700

    test: write test for uniform genetic algo

--- libs/genetic-algorithm/src/crossover/uniform.rs
+++ libs/genetic-algorithm/src/crossover/uniform.rs
@@ -29,3 +29,45 @@ impl CrossOverMethod for UniformCrossover {
             .collect()
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use rand::SeedableRng;
+    use rand_chacha::ChaCha8Rng;
+
+    use super::*;
+
+    // Verify that child is 50% parent a and 50% parent b
+    #[test]
+    fn child_equal_share_parents() {
+        let mut rng = ChaCha8Rng::from_seed(Default::default());
+        let parent_a: Chromosome = 
+            (1..=100)
+                .map(|n| n as f32)
+                .collect();
+        let parent_b: Chromosome = 
+            (1..=100)
+                .map(|n| -n as f32)
+                .collect();
+
+        let child = UniformCrossover::new()
+            .crossover(&mut rng, &parent_a, &parent_b);
+
+        // Number of genes different between `child` and `parent_a`
+        let diff_a = child
+            .iter()
+            .zip(parent_a)
+            .filter(|(c, p)| *c != p)
+            .count();
+
+        // Number of genes different between `child` and `parent_b`
+        let diff_b = child
+            .iter()
+            .zip(parent_b)
+            .filter(|(c, p)| *c != p)
+            .count();
+        
+        assert_eq!(diff_a, 49);
+        assert_eq!(diff_b, 51);
+    }
+}
