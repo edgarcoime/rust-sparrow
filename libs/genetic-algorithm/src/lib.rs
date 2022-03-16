6568574c303a10cd3d02e0cddcea1d624eb60553
Edgar Coime <e.coimeii@gmail.com>
2022-03-15 18:06:39 -0700

    doc: better ranking algorithm

--- libs/genetic-algorithm/src/lib.rs
+++ libs/genetic-algorithm/src/lib.rs
@@ -8,6 +8,9 @@ impl GeneticAlgorithm {
     pub fn evolve<T>(&self, population: &[T]) -> Vec<T> {
         // https://www.tutorialspoint.com/genetic_algorithms/genetic_algorithms_parent_selection.htm
         // Fitness algorithm to use
+        // TODO: Implement rank based selection
+        // https://setu677.medium.com/how-to-perform-roulette-wheel-and-rank-based-selection-in-a-genetic-algorithm-d0829a37a189
+        // Better selection would be rank based selection
         assert!(!population.is_empty())
 
         (0..population.len())
