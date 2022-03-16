ee2672543592bea299a5d12532f7eaf26778bfcb
Edgar Coime <e.coimeii@gmail.com>
2022-03-15 19:22:21 -0700

    feat: setup selection in genetic algorithm

--- libs/genetic-algorithm/src/lib.rs
+++ libs/genetic-algorithm/src/lib.rs
@@ -4,14 +4,43 @@ use self::{individual::*, selection::*};
 mod individual;
 mod selection;
 
-pub struct GeneticAlgorithm;
+pub struct GeneticAlgorithm<S> {
+    selection_method: S,
+}
 
-impl GeneticAlgorithm {
-    pub fn new() -> Self {
-        Self
+impl<S> GeneticAlgorithm<S>
+where
+    S: SelectionMethod
+{
+    pub fn new(selection_method: S) -> Self {
+        Self {
+            selection_method
+        }
     }
 
-    pub fn evolve<I>(&self, population: &[I]) -> Vec<I> {
-        todo!()
+    pub fn evolve<I>(&self, population: &[I]) -> Vec<I>
+    where
+        I: Individual
+    {
+        assert!(!population.is_empty());
+
+        let rng = rand
+        
+        (0..population.len())
+            .map(|_| {
+                // TODO: selection
+                let parent_a = self
+                    .selection_method
+                    .select(population);
+                
+                let parent_b = self
+                    .selection_method
+                    .select(population);
+
+                // TODO: crossover
+                // TODO: mutation
+                todo!()
+            })
+            .collect()
     }
 }
