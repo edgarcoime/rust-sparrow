9c4278a5cfaf7fc79cbed325d8ce11195d4ade0d
Edgar Coime <e.coimeii@gmail.com>
2022-03-15 23:43:11 -0700

    feat: draw basic canvas shape on on page

--- www/index.js
+++ www/index.js
@@ -6,3 +6,12 @@ const simulation = sim.Simulation.new();
 console.log(simulation.world());
 // const world = simulation.world();
 // console.log(world);
+
+const viewport = document.getElementById("viewport");
+const ctx = viewport.getContext("2d");
+
+// Determines color of the upcoming shape.
+ctx.fillStyle = 'rgb(255, 0, 0)';
+
+// Draws a rectangle filled with color determined by `fillStyle`
+ctx.fillRect(10, 10, 100, 50) // X Y W H
