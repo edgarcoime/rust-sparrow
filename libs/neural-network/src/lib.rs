ed524ff8e62e750d8739b597c5c4dff08be0348e
Edgar Coime <e.coimeii@gmail.com>
2022-03-17 14:02:29 -0700

    feat: impl from_weights for Neural

--- libs/neural-network/src/lib.rs
+++ libs/neural-network/src/lib.rs
@@ -3,6 +3,7 @@
 pub use self::layer_topology::*;
 use self::{layer::*, neuron::*};
 use rand::{Rng, RngCore};
+use std::iter::once;
 
 mod errors;
 mod layer;
@@ -17,6 +18,32 @@ pub struct Network {
 
 impl Network {
     crate fn new(layers: Vec<Layer>) -> Self {
+        Self { layers }
+    }
+
+    pub fn from_weights(
+        layers: &[LayerTopology],
+        weights: impl IntoIterator<Item = f32>,
+    ) -> Self {
+        assert!(layers.len() > 1);
+
+        let mut weights = weights.into_iter();
+
+        let layers = layers
+            .windows(2)
+            .map(|layers| {
+                Layer::from_weights(
+                    layers[0].neurons,
+                    layers[1].neurons,
+                    &mut weights,
+                )
+            })
+            .collect();
+        
+            if weights.next().is_some() {
+                panic!("got too many weights")
+            }
+
         Self { layers }
     }
 
@@ -41,15 +68,12 @@ impl Network {
             .fold(inputs, |inputs, layer| layer.propagate(inputs))
     }
 
-    pub fn weights(&self) -> Vec<f32> {
-        use std::iter::once;
-
+    pub fn weights(&self) -> impl Iterator<Item = f32> + '_ {
         self.layers
             .iter()
             .flat_map(|layer| layer.neurons.iter())
             .flat_map(|neuron| once(&neuron.bias).chain(&neuron.weights))
             .cloned()
-            .collect()
     }
 }
 
@@ -69,12 +93,31 @@ mod tests {
                 Layer::new(vec![Neuron::new(0.5, vec![0.6, 0.7, 0.8])]),
             ]);
 
-            let actual = network.weights();
-            let expected: Vec<f32> = vec![0.1, 0.2];
+            let actual: Vec<_> = network.weights().collect();
+            let expected: Vec<f32> = vec![0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8];
 
             approx::assert_relative_eq!(
                 actual.as_slice(),
                 expected.as_slice(),
+            );
+        }
+    }
+
+    mod from_weights {
+        use super::*;
+
+        #[test]
+        fn test() {
+            let layers = &[LayerTopology { neurons: 3 }, LayerTopology { neurons: 2 }];
+            let weights = vec![0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8];
+
+            let actual: Vec<_> = Network::from_weights(layers, weights.clone())
+                .weights()
+                .collect();
+            
+            approx::assert_relative_eq!(
+                actual.as_slice(),
+                weights.as_slice(),
             );
         }
     }
