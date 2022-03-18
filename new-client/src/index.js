c29f9a1cd7463a97f6cced276c8dad7518bec1b5
Edgar Coime <e.coimeii@gmail.com>
2022-03-18 11:40:17 -0700

    refactor: changed imports and webpack build config

--- new-client/src/index.js
+++ new-client/src/index.js
@@ -1,107 +1,105 @@
 import { Viewport } from "./app/viewport";
-import("lib-simulation-wasm").then((sim) => {
-  const viewport = new Viewport(document.getElementById("viewport"));
-  
-  /**
-   * Current simulation
-   *
-   * @type {Simulation}
-   */
-  let simulation = new sim.Simulation(sim.Simulation.default_config());
-  
-  /**
-   * Determines whether the simulation is working or not.
-   * Can be modified by pause command.
-   *
-   * @type {boolean}
-   */
-  let active = true;
-  
-  /**
-   *  Current configuration of running simulation
-   */
-  const config = simulation.config();
-  
-  document.getElementById("train").onclick = function() {
-    for (let i = 0; i < 50; i++) {
-      console.log(simulation.train())
+import * as sim from "lib-simulation-wasm";
+
+const viewport = new Viewport(document.getElementById("viewport"));
+
+/**
+ * Current simulation
+ *
+ * @type {Simulation}
+ */
+let simulation = new sim.Simulation(sim.Simulation.default_config());
+
+/**
+ * Determines whether the simulation is working or not.
+ * Can be modified by pause command.
+ *
+ * @type {boolean}
+ */
+let active = true;
+
+/**
+ *  Current configuration of running simulation
+ */
+const config = simulation.config();
+
+document.getElementById("train").onclick = function() {
+  for (let i = 0; i < 50; i++) {
+    console.log(simulation.train())
+  }
+}
+
+const redraw = () => {
+  if (active) {
+    const stats = simulation.step();
+
+    if (stats) {
+      console.log(stats);
     }
   }
+
+  const config = simulation.config();
+  const world = simulation.world();
+
+  viewport.clear();
+
+  for (const food of world.foods) {
+    viewport.drawCircle(
+      food.x,
+      food.y,
+      config.food_size / 2.0,
+      "rgb(0, 255, 120)"
+    );
+  }
+
+  for (const animal of world.animals) {
+    viewport.drawTriangle(
+      animal.x,
+      animal.y,
+      config.food_size,
+      animal.rotation,
+      'rgb(255, 255, 255)',
+    );
+
+    const anglePerCell = config.eye_fov_angle / config.eye_cells;
   
-  const redraw = () => {
-    if (active) {
-      const stats = simulation.step();
+    for (let cellId = 0; cellId < config.eye_cells; cellId++) {
+      const angleFrom = (animal.rotation - config.eye_fov_angle / 2.0) + (cellId * anglePerCell);
+      const angleTo = angleFrom + anglePerCell;
+      const energy = animal.vision[cellId];
   
-      if (stats) {
-        console.log(stats);
-      }
-    }
-  
-    const config = simulation.config();
-    const world = simulation.world();
-  
-    viewport.clear();
-  
-    for (const food of world.foods) {
-      viewport.drawCircle(
-        food.x,
-        food.y,
-        config.food_size / 2.0,
-        "rgb(0, 255, 120)"
-      );
-    }
-  
-    for (const animal of world.animals) {
-      viewport.drawTriangle(
+      viewport.drawArc(
         animal.x,
         animal.y,
-        config.food_size,
-        animal.rotation,
-        'rgb(255, 255, 255)',
-      );
-  
-      const anglePerCell = config.eye_fov_angle / config.eye_cells;
-    
-      for (let cellId = 0; cellId < config.eye_cells; cellId++) {
-        const angleFrom = (animal.rotation - config.eye_fov_angle / 2.0) + (cellId * anglePerCell);
-        const angleTo = angleFrom + anglePerCell;
-        const energy = animal.vision[cellId];
-    
-        viewport.drawArc(
-          animal.x,
-          animal.y,
-          (config.food_size * 2.5),
-          angleFrom,
-          angleTo,
-          `rgba(0, 255, 120, ${energy})`
-        )
-      }
-    }
-  
-    // requestAnimationFrame(redraw);
-  };
-  
-  // region     Animation
-  
-  var fps = 60;
-  var now;
-  var then = Date.now()
-  var interval = 1000 / fps;
-  var delta;
-  
-  function animationLoop() {
-    requestAnimationFrame(animationLoop);
-  
-    now = Date.now()
-    delta = now - then;
-  
-    if (delta > interval) {
-      then = now - (delta % interval);
-  
-      redraw()
+        (config.food_size * 2.5),
+        angleFrom,
+        angleTo,
+        `rgba(0, 255, 120, ${energy})`
+      )
     }
   }
-  
-  animationLoop();
-  // endregion: Animation
-})
+};
+
+
+// region     Animation
+var fps = 60;
+var now;
+var then = Date.now()
+var interval = 1000 / fps;
+var delta;
+
+function animationLoop() {
+  requestAnimationFrame(animationLoop);
+
+  now = Date.now()
+  delta = now - then;
+
+  if (delta > interval) {
+    then = now - (delta % interval);
+
+    redraw()
+  }
+}
+
+animationLoop();
+// endregion: Animation
--- new-client/webpack.prod.js
+++ new-client/webpack.prod.js
@@ -2,5 +2,8 @@ const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
+  stats: {
+    errorDetails: true,
+  },
   mode: 'production',
 });
