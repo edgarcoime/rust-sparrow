3ab15a737c7e0beba4c88227c2c84a9819c209e8
Edgar Coime <e.coimeii@gmail.com>
2022-03-18 02:12:11 -0700

    build: new client working

--- new-client/webpack.common.js
+++ new-client/webpack.common.js
@@ -1,5 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const WasmPackPlugin = require("@wasm-tool/wasm-pack-plugin");
+const webpack = require('webpack');
 
 module.exports = { 
   entry: {
@@ -13,6 +15,16 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: path.join(__dirname, "src/index.html")
-    })
+    }),
+    // new WasmPackPlugin({
+    //   crateDirectory: path.resolve(__dirname, "../libs/simulation-wasm")
+    // }),
+    // new webpack.ProvidePlugin({
+    //   TextEncoder: ["text-encoding", "TextEncoder"],
+    //   TextDecoder: ["text-encoding", "TextDecoder"],
+    // })
   ],
+  experiments: {
+    asyncWebAssembly: true,
+  }
 }
