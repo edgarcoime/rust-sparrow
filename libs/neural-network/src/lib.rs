399b342cba11c08c5f3d5efce26c62c8ac96e96c
Edgar Coime <e.coimeii@hotmail.com>
2024-11-15 22:57:54 -0800

    feat: add propagate

---
+++ libs/neural-network/src/lib.rs
@@ -0,0 +1,29 @@
+#[derive(Debug)]
+pub struct Network {
+    layers: Vec<Layer>,
+}
+
+impl Network {
+    pub fn propagate(&self, inputs: Vec<f32>) -> Vec<f32> {
+        self.layers
+            .iter()
+            .fold(inputs, |inputs, layer| layer.propagate(inputs))
+    }
+}
+
+#[derive(Debug)]
+struct Layer {
+    neurons: Vec<Neuron>,
+}
+
+impl Layer {
+    fn propagate(&self, inputs: Vec<f32>) -> Vec<f32> {
+        todo!()
+    }
+}
+
+#[derive(Debug)]
+struct Neuron {
+    bias: f32,
+    weights: Vec<f32>,
+}
