b5f877a45c26e65d94a41630c7b2b316f350d3b3
Edgar Coime <e.coimeii@hotmail.com>
2024-11-15 23:58:09 -0800

    feat: add basic tests for random and propagate

--- libs/neural-network/src/lib.rs
+++ libs/neural-network/src/lib.rs
@@ -1,5 +1,4 @@
-use rand::Rng;
-
+use rand::{Rng, RngCore};
 
 #[derive(Debug)]
 pub struct LayerTopology {
@@ -12,13 +11,13 @@ pub struct Network {
 }
 
 impl Network {
-    pub fn random(layers: &[LayerTopology]) -> Self {
+    pub fn random(rng: &mut dyn RngCore, layers: &[LayerTopology]) -> Self {
         // Layer with 1 is doable bet doesnt make sense
         assert!(layers.len() > 1);
 
         let layers = layers
             .windows(2)
-            .map(|layers| Layer::random(layers[0].neurons, layers[1].neurons))
+            .map(|layers| Layer::random(rng, layers[0].neurons, layers[1].neurons))
             .collect();
 
         Self { layers }
@@ -37,9 +36,9 @@ struct Layer {
 }
 
 impl Layer {
-    fn random(input_size: usize, ouput_size: usize) -> Self {
+    fn random(rng: &mut dyn RngCore, input_size: usize, ouput_size: usize) -> Self {
         let neurons = (0..ouput_size)
-            .map(|_| Neuron::random(input_size))
+            .map(|_| Neuron::random(rng, input_size))
             .collect();
 
         Self { neurons }
@@ -60,13 +59,10 @@ struct Neuron {
 }
 
 impl Neuron {
-    fn random(input_size: usize) -> Self {
-        let mut rng = rand::thread_rng();
+    fn random(rng: &mut dyn RngCore, input_size: usize) -> Self {
         let bias = rng.gen_range(-1.0..=1.0);
 
-        let weights = (0..input_size)
-            .map(|_| rng.gen_range(-1.0..=1.0))
-            .collect();
+        let weights = (0..input_size).map(|_| rng.gen_range(-1.0..=1.0)).collect();
 
         Self { bias, weights }
     }
@@ -83,5 +79,42 @@ impl Neuron {
             .sum::<f32>();
 
         (self.bias + ouput).max(0.0)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use approx::assert_relative_eq;
+    use rand::SeedableRng;
+    use rand_chacha::ChaCha8Rng;
+
+    #[test]
+    fn random() {
+        let mut rng = ChaCha8Rng::from_seed(Default::default());
+        let neuron = Neuron::random(&mut rng, 4);
+
+        assert_relative_eq!(neuron.bias, -0.6255188);
+        assert_relative_eq!(
+            neuron.weights.as_slice(),
+            &[0.67383957, 0.8181262, 0.26284897, 0.5238807].as_ref()
+        );
+    }
+
+    #[test]
+    fn propagate() {
+        let neuron = Neuron {
+            bias: 0.5,
+            weights: vec![-0.3, 0.8],
+        };
+
+        // ensure `.max()` (our ReLU) works:
+        assert_relative_eq!(neuron.propagate(&[-10.0, -10.0]), 0.0,);
+
+        // `0.5` and `1.0` is a fair dice roll
+        assert_relative_eq!(
+            neuron.propagate(&[0.5, 1.0]),
+            (0.5 * -0.3) + (1.0 * 0.8) + 0.5,
+        );
     }
 }
