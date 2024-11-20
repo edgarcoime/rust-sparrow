fd83d44c044a1da7a03932a08b53b7dcd596ca90
Edgar Coime <e.coimeii@hotmail.com>
2024-11-20 13:47:20 -0800

    test: genetic algorithm using histogram

--- libs/genetic-algorithm/src/selection/roulette_wheel.rs
+++ libs/genetic-algorithm/src/selection/roulette_wheel.rs
@@ -12,3 +12,42 @@ impl SelectionMethod for RouletteWheelSelection {
             .expect("population is empty")
     }
 }
+
+
+#[cfg(test)]
+mod tests {
+    use crate::*;
+    use rand::SeedableRng;
+    use rand_chacha::ChaCha8Rng;
+    use std::collections::BTreeMap;
+    use std::iter::FromIterator;
+
+
+    #[test]
+    fn roulette_wheel_selection() {
+        let mut rng = ChaCha8Rng::from_seed(Default::default());
+
+        let population = vec![
+            TestIndividual::new(2.0),
+            TestIndividual::new(1.0),
+            TestIndividual::new(4.0),
+            TestIndividual::new(3.0),
+        ];
+
+        let actual_histogram = (0..1000)
+            .map(|_| RouletteWheelSelection.select(&mut rng, &population))
+            .fold(BTreeMap::default(), |mut histogram, individual| {
+                *histogram.entry(individual.fitness() as i32).or_default() += 1;
+                histogram
+            });
+
+        let expected_histogram = BTreeMap::from_iter([
+            (1, 98),
+            (2, 202),
+            (3, 278),
+            (4, 422),
+        ]);
+
+        assert_eq!(actual_histogram, expected_histogram);
+    }
+}
