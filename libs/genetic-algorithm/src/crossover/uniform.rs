fd4ed8e4319093689f97c3240ff1dfe1a7c417c1
Edgar Coime <e.coimeii@hotmail.com>
2024-11-20 14:29:28 -0800

    feat: uniform crossover implementation

--- libs/genetic-algorithm/src/crossover/uniform.rs
+++ libs/genetic-algorithm/src/crossover/uniform.rs
@@ -2,3 +2,20 @@ use crate::*;
 
 #[derive(Clone, Debug)]
 pub struct UniformCrossover;
+
+impl CrossoverMethod for UniformCrossover {
+    fn crossover(
+        &self,
+        rng: &mut dyn RngCore,
+        parent_a: &Chromosome,
+        parent_b: &Chromosome,
+    ) -> Chromosome {
+        assert!(parent_a.len() == parent_b.len());
+
+        parent_a
+            .iter()
+            .zip(parent_b.iter())
+            .map(|(&a, &b)| if rng.gen_bool(0.5) { a } else { b })
+            .collect()
+    }
+}
